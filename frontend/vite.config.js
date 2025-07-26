f7517fd21b0bc2975f9fb6538eb24f72883f7662
Divvala Harshavardhan Sai <harshasai3003@gmail.com>
2025-07-27 01:42:13 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { // <-- UPDATED BUILD CONFIGURATION
-    outDir: '../backend/dist', // Output to the 'dist' folder inside 'backend' folder (relative to 'frontend')
-    emptyOutDir: true, // Clear the output directory before building
-  }
 })
